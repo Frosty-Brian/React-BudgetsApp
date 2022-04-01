5e14e936a29055df2067e3e8ced7b64ae062083b
Frosty-Brian <64714386+Frosty-Brian@users.noreply.github.com>
2022-03-31 21:16:58 -0700

    add BudgetsContext

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -13,12 +13,31 @@ export const BudgetsProvider = ({ children }) => {
   function getBudgetExpenses(budgetId) {
     return expenses, filter((expense) => expense.budgetId === budgetId);
   }
-  function addExpense() {}
-  function addBudget() {
-    setBudgets((prevBudgets) => {});
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
   }
-  function deleteBudget() {}
-  function deleteExpense() {}
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      // if name already exists don't create it
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+  function deleteBudget({ id }) {
+    //TODO: deal with uncategorized expenses.
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id);
+    });
+  }
+  function deleteExpense({ id }) {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id);
+    });
+  }
   return (
     <BudgetsContext.Provider
       value={{
